19ad7744a61f32a079d59d782e7bf73ef37eabf8
Luis Eduardo Orjuela Rojas <luchorjuela@hotmail.com>
2025-03-08 17:29:34 -0500

    add gameover screen when player die

---
+++ Assets/_Project/Scripts/Audio/GameplayerGameOverControlleer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Surviblewilderness
+{
+    public class GameplayerGameOverControlleer : MonoBehaviour
+    {
+        [SerializeField] private IDamageable damageable;
+
+        [SerializeField] private GameOverMenuUiManager gameOverMenuUi;
+
+        private void Awake ()
+        {
+            damageable = GetComponent<IDamageable>();
+        }
+
+        private void OnEnable ()
+        {
+            if (damageable != null)
+                damageable.OnDamageRecieved += OnDamageRecieved;
+
+        }
+
+        private void OnDisable ()
+        {
+            if (damageable != null)
+                damageable.OnDamageRecieved -= OnDamageRecieved;
+        }
+
+        private void OnDamageRecieved (int _)
+        {
+            if (damageable.health > 0)
+                return;
+            gameOverMenuUi.Show();
+        }
+    }
+}
