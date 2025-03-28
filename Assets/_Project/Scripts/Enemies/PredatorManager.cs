e64f89394c91aada5d19628ca8631b71b817044b
Smit Patel <patelsitu2041@gmail.com>
2025-03-28 18:23:04 -0400

    Predator Load completed
    - Instantiating predator game object on load from predator save manager

--- Assets/_Project/Scripts/Enemies/PredatorManager.cs
+++ Assets/_Project/Scripts/Enemies/PredatorManager.cs
@@ -1,20 +1,31 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Surviblewilderness
 {
     public class PredatorManager : MonoBehaviour
     {
-
+        //private static Dictionary <GameObject, Transform> currentActivePredator = new Dictionary<GameObject, Transform>();
         private void Awake()
         {
             //registering this predator into a static list of predators for save 
             PredatorSaveManager.RegisterPredator(this.transform);
+            //currentActivePredator.Add(this.gameObject, this.transform);
         }
 
         private void OnDestroy()
         {
             //unregister/remove this predator from the static list of predator  
-            PredatorSaveManager.UnregisterPredator(this.transform); 
+            PredatorSaveManager.UnregisterPredator(this.transform);
+           // currentActivePredator.Remove(this.gameObject);
         }
+
+        //public Dictionary<GameObject, Transform> GetPredators()
+        //{
+        //    return currentActivePredator;
+        //}   
+
+        //public void ReloadPredators(Dictionary<>)
     }
 }
